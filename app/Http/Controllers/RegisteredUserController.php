27450797022b0a7d890fff37365269d2a0e4d421
Kingmakerdee <denzelndlovu63@gmail.com>
2025-01-29 00:43:28 +0200

    Login and Registration system part two

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password as RulesPassword;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,21 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', RulesPassword::min(6), 'confirmed'], // password_confirmation
+        ]);
+
+        // create user
+        $user = User::create($attributes);
+
+        //log in
+        Auth::login($user);
+
+        // redirect somewhere
+        return redirect('/jobs');
     }
 }
