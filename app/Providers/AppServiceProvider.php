cce3915c27fcca03049229bc05f9fff80da3435c
Kingmakerdee <denzelndlovu63@gmail.com>
2025-01-29 15:41:41 +0200

    Add Job Policy for auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::preventLazyLoading();
 
-        Gate::define('edit-job', function (User $user, Job $job){
-            return $job->employer->user->is($user);
-        });
+ //       Gate::define('edit-job', function (User $user, Job $job){
+ //           return $job->employer->user->is($user);
+ //       });
 
     }
 }
